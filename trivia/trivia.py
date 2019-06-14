86de2b66d6ded4195a5fbfb5eeff6fbcf81bd016
MatteCatte <john@ivison.org>
2019-06-14 01:28:07 -0700

    Added new git started

--- trivia/trivia.py
+++ trivia/trivia.py
@@ -1,35 +1,82 @@
+#! /usr/bin/python3
+
 import requests
 import html
 import random
 import os, sys
 
+arg_whitelist = ["amount", "category", "difficulty", "type"]
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 # Sample API call
 # https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple
 
-amount = int(sys.argv[1][0])
-category = None
+class TriviaError(Exception):
+    def __init__(self, context, **kwargs):
+        if context == "bad_argument":
+            super().__init__(f"play trivia: {bcolors.FAIL}bad argument '{kwargs['argument']}'{bcolors.ENDC}")
+        elif context == "bad_argument_value":
+            super().__init__(f"play trivia: {bcolors.FAIL}bad argument value '{kwargs['value']}' for argument '{kwargs['argument']}'{bcolors.ENDC}")
+        elif context == "bad_response":
+            super().__init__(f"play trivia: {bcolors.FAIL}bad response '{kwargs['code']}{bcolors.ENDC}")
+        elif context == "internet":
+            super().__init__(f"play trivia: {bcolors.OKBLUE}bad connection, check that you are connected to the internet{bcolors.ENDC}")
 
+def vetArgValue(arg, value):
+    try:
+        if arg == "amount":
+            if int(value) < 1 or int(value) > 25:
+                raise Exception
+        elif arg == "category":
+            if int(value) < 9 or int(value) > 32:
+                raise Exception
+        elif arg == "difficulty":
+            if value not in ["easy", "medium", "hard"]:
+                raise Exception
+        elif arg == "type":
+            if value not in ["boolean", "multiple"]:
+                raise Exception
+                    
+    except Exception:
+        raise TriviaError("bad_argument_value", value=value, argument=arg)
+        return False
+    
+    else:
+        return True
 
 def get_questions(params):
     url_string = u"https://opentdb.com/api.php"
 
-    response = requests.get(
-        url_string,
-        params=params
-    )
+    try:
+        response = requests.get(
+            url_string,
+            params=params
+        )
+    except Exception:
+        raise TriviaError("internet")
 
     if response.status_code == 200:
         return response.json()
     else:
         print(f"An error occured, status code: {response.status_code}")
+        raise TriviaError("bad_response", code=response.status_code)
         return None
 
-def end_screen(amount_correct):
-    print(f"You scored {amount_correct}/{amount} (%{round(amount_correct/amount * 100)})")
+def end_screen(scoreArray):
+    print(f"You scored {sum(scoreArray)}/{len(scoreArray)} (%{round(sum(scoreArray)/len(scoreArray) * 100)})")
 
 def ask(response):
     
-    amount_correct = 0    
+    score = []    
 
     for i in range(len(response["results"])):
 
@@ -58,6 +105,9 @@ def ask(response):
             
             guess = input(f"{prompt}> ")
 
+            if guess == "quit" or guess == "q":
+                quit()
+
             try:
                 guess = int(guess)
             except Exception as e:
@@ -69,7 +119,9 @@ def ask(response):
                 print("Correct!" if (all_answers[guess] == answers["correct"]) else f"Wrong, correct answer: {answers['correct']}")
 
                 if (all_answers[guess] == answers["correct"]):
-                    amount_correct += 1
+                    score.append(1)
+                else:
+                    score.append(0)
 
                 input("\nEnter anything to continue...")
                 break
@@ -77,15 +129,39 @@ def ask(response):
             else:
                 prompt = "invalid answer"
 
-    end_screen(amount_correct)
+    end_screen(score)
 
 
-    
 
 
-        
-        
+def printHelp():
+    print("""
+play trivia help
+================
 
-ask(get_questions({"amount" : amount}))
+trivia is a play which uses opentdb's trivia database to ask some trivia questions
+
+enter the index of the answer you wish to give when the question appears
+enter 'q' or 'quit' to end it without scoring
 
 
+trivia arguments are passed like Python kwargs, like so:
+
+play trivia <argument>=<value>
+
+
+arguments:
+
+    amount...............................the number of questions the API returns, restricted between 1 and 25, defaults to 5
+
+    category.............................what category of questions the API returns, restricted between 9 and 32
+
+    difficulty...........................what difficulty of questions the API returns, restricted to either 'easy', 'medium'
+
+    type.................................can either be multiple (multiple choice) or boolean (true/false)
+
+
+
+
+
+""")
